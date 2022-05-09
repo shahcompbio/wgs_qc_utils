328d8d5393ff9e875dc136ab4734859cbc196023
Seongmin Choi <soymintc@gmail.com>
2022-05-09 12:09:46 -0400

    read chromosomes as strings

--- wgs_qc_utils/reader/read_remixt.py
+++ wgs_qc_utils/reader/read_remixt.py
@@ -3,10 +3,9 @@ import os
 import yaml
 
 def read(remixt):
-    cnv_data = pd.read_csv(remixt, sep="\t")
+    cnv_data = pd.read_csv(remixt, sep="\t", converters={'chromosome': str})
 
     cnv_data["total_raw_e"] = cnv_data.major_raw_e + cnv_data.minor_raw_e
-    cnv_data["chromosome"] = cnv_data.chromosome.astype(str) # object -> str
     cnv_data["chromosome"] = cnv_data.chromosome.str.lower()
     cnv_data.rename(columns={"chromosome":"chrom"}, inplace=True)
     
