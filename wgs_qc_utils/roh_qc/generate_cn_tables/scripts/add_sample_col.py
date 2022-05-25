4d61c317997e433f4fdb56e77efe94cd5daa35ea
Seongmin Choi <soymintc@gmail.com>
2022-05-25 17:36:16 -0400

    cleanup

--- wgs_qc_utils/roh_qc/generate_cn_tables/scripts/add_sample_col.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-sample = snakemake.wildcards[0]
-
-df = pd.read_csv(snakemake.input[0], dtype='str')
-df['sample'] = sample
-df.to_csv(str(snakemake.output[0]), index=False)
--- wgs_qc_utils/roh_qc/generate_cn_tables/scripts/merge_csvs.py
+++
@@ -1,5 +0,0 @@
-import dask.dataframe as dd
-
-
-df = dd.read_csv(snakemake.input, dtype='str')
-df.to_csv(str(snakemake.output[0]), index=False, single_file=True)
